83477117b730e7701632cacf6eb4dfa14896f1c9
laurpaik <laurpaik@gmail.com>
2017-02-17 10:25:58 -0500

    Add relationship to athlete serializer and model

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Athlete < ApplicationRecord
+  has_many :workouts, through: :logs
+  has_many :logs
+
   validates :given_name, presence: true
   validates :surname, presence: true
   validates :date_of_birth, presence: true
--- app/serializers/athlete_serializer.rb
+++ app/serializers/athlete_serializer.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class AthleteSerializer < ActiveModel::Serializer
-  attributes :id, :given_name, :surname, :date_of_birth
+  attributes :id, :given_name, :surname, :date_of_birth, :workouts
+
+  def workouts
+    object.workouts.pluck(:id)
+  end
 end
