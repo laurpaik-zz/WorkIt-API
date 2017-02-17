e2e846773e2b29334211adb04355907a8f8773ce
laurpaik <laurpaik@gmail.com>
2017-02-17 17:45:06 -0500

    Foreign key for athletes and users gets added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217140857) do
+ActiveRecord::Schema.define(version: 20170217163253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20170217140857) do
     t.date     "date_of_birth", null: false
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_athletes_on_user_id", using: :btree
   end
 
   create_table "examples", force: :cascade do |t|
@@ -57,6 +59,7 @@ ActiveRecord::Schema.define(version: 20170217140857) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "athletes", "users"
   add_foreign_key "examples", "users"
   add_foreign_key "logs", "athletes"
   add_foreign_key "logs", "workouts"
