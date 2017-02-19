9239ea09737e4c2fa203b43d9d748205cf26f1f4
laurpaik <laurpaik@gmail.com>
2017-02-19 13:36:25 -0500

    Create relationship between log and user
    -for authorization reasons

--- app/models/log.rb
+++ app/models/log.rb
@@ -3,6 +3,9 @@
 class Log < ApplicationRecord
   belongs_to :athlete
   belongs_to :workout
+  # belongs_to :user
+  delegate :logs, to: :athlete
+
   validates :athlete, presence: true
   validates :workout, presence: true
   validates :date_completed, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   include Authentication
   has_many :examples
   has_one :athlete
+  has_many :logs, through: :athlete
 end
