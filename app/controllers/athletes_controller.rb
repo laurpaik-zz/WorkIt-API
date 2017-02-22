906b5af31969a44d6da297baba2bded5eefa2d69
laurpaik <laurpaik@gmail.com>
2017-02-22 13:45:41 -0500

    Re-add index for testing reasons
    -needed to test if athlete was getting created if user was not
    -we Gucci

--- app/controllers/athletes_controller.rb
+++ app/controllers/athletes_controller.rb
@@ -12,6 +12,17 @@ class AthletesController < OpenReadController
     end
   end
 
+  def index
+    @athletes = Athlete.all
+
+    render json: @athletes
+  end
+
+  # GET /athletes/1
+  def show
+    render json: Athlete.find(params[:id])
+  end
+
   # Use callbacks to share common setup or constraints between actions.
   def set_athlete
     @athlete = Athlete.find_by(id: params[:id], user: current_user)
